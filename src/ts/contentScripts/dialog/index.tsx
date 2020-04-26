049177e43da998e0a98edabc6812ce82b8736aef
Drahoslav Zan <zandrahoslav@gmail.com>
2020-04-26 15:47:58 +0200

    styles

--- src/ts/contentScripts/dialog/index.tsx
+++ src/ts/contentScripts/dialog/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { v4 as uuid } from 'uuid';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { StyleSheetManager } from 'styled-components';
 import { Store } from 'webext-redux';
 import App from './containers/App';
 import { IDialogMessage } from './IMessage';
@@ -15,12 +16,18 @@ chrome.runtime.onMessage.addListener(async function(request: IDialogMessage) {
 	switch(request.dialog) {
 	case 'OPEN':
 		removeDomAnchor(id);
-		const elem = createDomAnchor(id, styles);
+		const root = createDomAnchor(id, styles);
+		const innerStyles = document.createElement('div');
+		const elem = document.createElement('div');
+		root.appendChild(innerStyles);
+		root.appendChild(elem);
 		await store.ready();
 		ReactDOM.render(
-			<Provider store={store}>
-				<App dialogId={id} />
-			</Provider>
+			<StyleSheetManager target={innerStyles}>
+				<Provider store={store}>
+					<App dialogId={id} />
+				</Provider>
+			</StyleSheetManager>
 			, elem);
 		break;
 	case 'CLOSE':
--- src/ts/scripts/dom.ts
+++ src/ts/scripts/dom.ts
@@ -1,10 +1,10 @@
 import retargetEvents from 'react-shadow-dom-retarget-events';
 
-export const createDomAnchor = (id: string, style?: string): HTMLElement => {
+export const createDomAnchor = (id: string, style?: string): ShadowRoot => {
 	const anchor = document.createElement('div');
 	anchor.id = id;
 	const shadow = anchor.attachShadow({mode: "open"});
-	document.body.insertBefore(anchor, document.body.childNodes[0]);
+	document.body.insertBefore(anchor, document.body.nextSibling);
 	if (style) {
 		const linkElem = document.createElement('link');
 		linkElem.setAttribute('rel', 'stylesheet');
@@ -12,9 +12,7 @@ export const createDomAnchor = (id: string, style?: string): HTMLElement => {
 		shadow.appendChild(linkElem);
 	}
 	retargetEvents(shadow);
-	const elem = document.createElement('div');
-	shadow.appendChild(elem);
-	return elem;
+	return shadow;
 };
 
 export const removeDomAnchor = (id: string) => {
