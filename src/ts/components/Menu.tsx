db5b936cf5bbeaa4b95b96a113871ada0bc8e486
Drahoslav Zan <zandrahoslav@gmail.com>
2020-04-28 11:59:45 +0200

    fix

--- src/ts/components/Menu.tsx
+++ src/ts/components/Menu.tsx
@@ -10,15 +10,16 @@ interface IMenuProps {
 interface IButton {
 	label: string;
 	selected: boolean;
+	last?: boolean;
 	onClick(): void;
 }
 
 const Button = (props: IButton) => {
 	return (
-		<li className="flex-1 mr-2">
+		<li className={`flex-1 ${props.last ? '' : 'mr-2'}`}>
 			{props.selected
-				? <a onClick={props.onClick} className="text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white">{props.label}</a>
-				: <a onClick={props.onClick} className="text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4">{props.label}</a>
+				? <button onClick={props.onClick} className="block w-full border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white">{props.label}</button>
+				: <button onClick={props.onClick} className="block w-full border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4">{props.label}</button>
 			}
 		</li>
 	);
@@ -33,7 +34,7 @@ const Menu = (props: IMenuProps) => {
 		<ul className="flex">
 			<Button onClick={() => handleSelected('synonyms')} selected={props.selection === 'synonyms'} label="Synonyms" />
 			<Button onClick={() => handleSelected('antonyms')} selected={props.selection === 'antonyms'} label="Antonyms" />
-			<Button onClick={() => handleSelected('sentences')} selected={props.selection === 'sentences'} label="Sentences" />
+			<Button onClick={() => handleSelected('sentences')} selected={props.selection === 'sentences'} label="Sentences" last />
 		</ul>
 	);
 };
